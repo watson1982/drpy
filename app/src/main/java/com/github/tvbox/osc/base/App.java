55c7320909ce3b59f029e17b618593c4bb68fc73
watson1982 <112817572+watson1982@users.noreply.github.com>
2023-10-07 00:52:15 +0800

    App

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -1,26 +1,33 @@
 package com.github.tvbox.osc.base;
 
 import android.app.Activity;
+
 import androidx.multidex.MultiDexApplication;
+
 import com.github.catvod.crawler.JsLoader;
+import com.github.tvbox.osc.R;
 import com.github.tvbox.osc.callback.EmptyCallback;
 import com.github.tvbox.osc.callback.LoadingCallback;
 import com.github.tvbox.osc.data.AppDataManager;
 import com.github.tvbox.osc.server.ControlManager;
-import com.github.tvbox.osc.util.FileUtils;
 import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.LOG;
-import com.github.tvbox.osc.util.js.jianpian;
+import com.github.tvbox.osc.util.LocaleHelper;
 import com.github.tvbox.osc.util.OkGoHelper;
 import com.github.tvbox.osc.util.PlayerHelper;
+import com.github.tvbox.osc.util.js.jianpian;
+
 import com.kingja.loadsir.core.LoadSir;
-import com.jieba_android.JiebaSegmenter;
 import com.orhanobut.hawk.Hawk;
 import com.p2p.P2PClass;
-import com.whl.quickjs.android.QuickJSLoader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import com.jieba_android.JiebaSegmenter;
+
+import com.simple.spiderman.SpiderMan;
+import com.whl.quickjs.android.QuickJSLoader;
 
 import me.jessyan.autosize.AutoSizeConfig;
 import me.jessyan.autosize.unit.Subunits;
@@ -41,58 +48,67 @@ public class App extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        //放在其他库初始化前
+        SpiderMan.init(this).setTheme(R.style.SpiderManTheme_Dark);
+
         initParams();
+        // takagen99 : Initialize Locale
+        initLocale();
         // OKGo
         OkGoHelper.init();
-
         // 初始化Web服务器
         ControlManager.init(this);
         //初始化数据库
         AppDataManager.init();
         LoadSir.beginBuilder()
-            .addCallback(new EmptyCallback())
-            .addCallback(new LoadingCallback())
-            .commit();
-        AutoSizeConfig.getInstance()
-            .setCustomFragment(true)
-            .getUnitsManager()
-            .setSupportDP(false)
-            .setSupportSP(false)
-            .setSupportSubunits(Subunits.MM);
+                .addCallback(new EmptyCallback())
+                .addCallback(new LoadingCallback())
+                .commit();
+        AutoSizeConfig.getInstance().setCustomFragment(true).getUnitsManager()
+                .setSupportDP(false)
+                .setSupportSP(false)
+                .setSupportSubunits(Subunits.MM);
         PlayerHelper.init();
         QuickJSLoader.init();
-        FileUtils.cleanPlayerCache();
+
         JiebaSegmenter.init(instance.getApplicationContext());
     }
 
     private void initParams() {
         // Hawk
-        Hawk.init(this)
-            .build();
+        Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
-        
+
+        putDefault(HawkConfig.HOME_REC, 2);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
+        putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
+        putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
+//        putDefault(HawkConfig.HOME_NUM, 2);       // History Number
+//        putDefault(HawkConfig.DOH_URL, 2);        // DNS
+//        putDefault(HawkConfig.SEARCH_VIEW, 1);    // Text or Picture
         String defaultApiName = "默认-自备份线路";
         String defaultApi = "";
-        
+
         HashMap<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());
         defaultApiMap.put(defaultApiName, defaultApi);
 
         ArrayList<String> defaultApiHistory = Hawk.get(HawkConfig.API_NAME_HISTORY, new ArrayList<>());
         defaultApiHistory.add(defaultApiName);
-        
+
+        Hawk.put(HawkConfig.DEBUG_OPEN, false);
+
         putDefault(HawkConfig.API_URL, defaultApi);
         putDefault(HawkConfig.API_NAME, defaultApiName);
         putDefault(HawkConfig.API_NAME_HISTORY, defaultApiHistory);
         putDefault(HawkConfig.API_MAP, defaultApiMap);
-
-        //        putDefault(HawkConfig.HOME_REC, 1);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
-        putDefault(HawkConfig.PLAY_TYPE, 1); // Player   0=系统, 1=IJK, 2=Exo
-        putDefault(HawkConfig.IJK_CODEC, "硬解码"); // IJK Render 软解码, 硬解码
-        putDefault(HawkConfig.PLAY_RENDER, 1); // 渲染 0=text, 1=surface
-        //        putDefault(HawkConfig.DOH_URL, 2);        // DNS
-        //        putDefault(HawkConfig.SEARCH_VIEW, 1);    // Text or Picture
     }
 
+    private void initLocale() {
+        if (Hawk.get(HawkConfig.HOME_LOCALE, 0) == 0) {
+            LocaleHelper.setLocale(App.this, "zh");
+        } else {
+            LocaleHelper.setLocale(App.this, "");
+        }
+    }
     public Activity getHomeActivity() {
         return homeActivity;
     }
@@ -101,9 +117,15 @@ public class App extends MultiDexApplication {
         this.homeActivity = homeActivity;
     }
 
-    private void putDefault(String key, Object value) {
-        if (!Hawk.contains(key)) {
-            Hawk.put(key, value);
+    public static P2PClass getp2p() {
+        try {
+            if (p == null) {
+                p = new P2PClass(instance.getExternalCacheDir().getAbsolutePath());
+            }
+            return p;
+        } catch (Exception e) {
+            LOG.e(e);
+            return null;
         }
     }
 
@@ -114,26 +136,21 @@ public class App extends MultiDexApplication {
     @Override
     public void onTerminate() {
         super.onTerminate();
-        JsLoader.stopAll();
+        JsLoader.load();
         jianpian.finish();
     }
 
-    public static P2PClass getp2p() {
-        try {
-            if (p == null) {
-                p = new P2PClass(instance.getCacheDir()
-                    .getAbsolutePath());
-            }
-            return p;
-        } catch (Exception unused) {
-            return null;
+    private void putDefault(String key, Object value) {
+        if (!Hawk.contains(key)) {
+            Hawk.put(key, value);
         }
     }
-
+    
     public void setDashData(String data) {
         dashData = data;
     }
     public String getDashData() {
         return dashData;
     }
+
 }
