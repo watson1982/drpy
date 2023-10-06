9213535a7533d7f05609b5fe1ac5243653c66dde
watson1982 <112817572+watson1982@users.noreply.github.com>
2023-10-06 18:43:49 +0800

    Update HomeHotVodAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
@@ -35,7 +35,7 @@ public class HomeHotVodAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHol
         ImageView ivThumb = helper.getView(R.id.ivThumb);
         //由于部分电视机使用glide报错
         if (!TextUtils.isEmpty(item.pic)) {
-            ImgUtil.load(mVideo.pic, ivThumb, 10);
+            ImgUtil.load(item.pic, ivThumb, 10);
         } else {
             ivThumb.setImageResource(R.drawable.img_loading_placeholder);
         }
