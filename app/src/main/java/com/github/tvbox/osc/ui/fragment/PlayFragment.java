cc714f521d790428c06c7127fd00e3db06e5abf6
watson1982 <112817572+watson1982@users.noreply.github.com>
2023-10-06 23:06:36 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -893,8 +893,8 @@ public class PlayFragment extends BaseLazyFragment {
 
         stopParse();
         if (mVideoView != null) mVideoView.release();
-        String subtitleCacheKey = mVodInfo.sourceKey + "-" + mVodInfo.id + "-" + mVodInfo.playFlag + "-" + mVodInfo.playIndex+ "-" + vs.name + "-subt";
-        String progressKey = mVodInfo.sourceKey + mVodInfo.id + mVodInfo.playFlag + mVodInfo.playIndex;
+        subtitleCacheKey = mVodInfo.sourceKey + "-" + mVodInfo.id + "-" + mVodInfo.playFlag + "-" + mVodInfo.playIndex+ "-" + vs.name + "-subt";
+        progressKey = mVodInfo.sourceKey + mVodInfo.id + mVodInfo.playFlag + mVodInfo.playIndex;
         //重新播放清除现有进度
         if (reset) {
             CacheManager.delete(MD5.string2MD5(progressKey), 0);
@@ -920,8 +920,8 @@ public class PlayFragment extends BaseLazyFragment {
                 }
             }
             
-            public void list(String playList) {
-            }
+            @Override
+            public void list(Map < Integer, String > urlMap) {}
 
             @Override
             public void play(String url) {
